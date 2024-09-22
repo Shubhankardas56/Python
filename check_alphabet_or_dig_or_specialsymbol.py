7cc80d7fd778c26e2eefe1f246cd21e05d77102b
Shubhankar Das <59699880+Shubhankardas56@users.noreply.github.com>
2024-09-22 19:01:34 +0530

    Delete check_alphabet_or_dig_or_specialsymbol.py

--- check_alphabet_or_dig_or_specialsymbol.py
+++
@@ -1,8 +0,0 @@
-# character is digit or alphabet or symbol
-char=input('enter a single character: ')
-if char.isdigit():
-    print('entered character is a digit')
-elif char.isalpha():
-    print('entered character is a alphabet')
-else:
-    print('entered character is a specialsymbool')
