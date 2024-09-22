cd33784835e396a26fe3354b53505e680aede648
Shubhankar Das <59699880+Shubhankardas56@users.noreply.github.com>
2024-09-22 19:01:59 +0530

    Delete check_upper_or_lower.py

--- check_upper_or_lower.py
+++
@@ -1,6 +0,0 @@
-# check alphabet is in uppercase or lowercase
-alph=input('Enter an alphabet: ')
-if alph.isupper():
-    print('Entred alphabet is in uppercase')
-else:
-    print('Entred alphabet is in lowercase')
