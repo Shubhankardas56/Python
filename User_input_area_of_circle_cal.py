2fb640e5fb6fb04ccbb6bc9fffc6fe2efb5735f7
Shubhankar Das <59699880+Shubhankardas56@users.noreply.github.com>
2024-09-22 19:00:46 +0530

    Delete User_input_area_of_circle_cal.py

--- User_input_area_of_circle_cal.py
+++
@@ -1,7 +0,0 @@
-# area of circle calculater-user_input
-radius=float(input('Enter radius of circle: '))
-def area_of_circle():
-    r=radius
-    area=3.1416*(r*r)
-    return area
-print('area of circle is: ',area_of_circle())
