bede8f279b255e27027c2954080784f98a06bc16
Shubhankar Das <59699880+Shubhankardas56@users.noreply.github.com>
2024-09-22 18:57:45 +0530

    Delete Area_of_rectangle.py

--- Area_of_rectangle.py
+++
@@ -1,7 +0,0 @@
-#area of rectangle
-length=float(input('Enter length of rectangle: '))
-height=float(input('Enter height of rectangle: '))
-def rec_area():
-    area=length*height
-    print(f'area of rectangle using length {length} and height {height} is: {area}')
-rec_area()
