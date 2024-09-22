36c8a96a2c5d5e3704c8cda7e85957e505dca4ff
Shubhankar Das <59699880+Shubhankardas56@users.noreply.github.com>
2024-09-22 19:03:48 +0530

    Delete vowel_or_consonant.py

--- vowel_or_consonant.py
+++
@@ -1,7 +0,0 @@
-# check alphabet is vowel or consonant
-alph=input('Enter a alphabet: ')
-alphabet=alph.lower()
-if alphabet=='a' or alphabet=='e' or alphabet=='i' or alphabet=='o' or alphabet=='u':
-    print('Entred alphabet is vowel')
-else:
-    print('Enterd alphabet is consonant')
